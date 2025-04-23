d0d9c3be4f6e4769a3a0a75edd7b883f5a8ca67a
yaelb <yaelb@BOOK-Q2S07FB0VB>
2025-04-23 18:09:40 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GenerateurDeScenario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerer = new System.Windows.Forms.Button();
+            this.rtbScenario = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerer
+            // 
+            this.btnGenerer.Location = new System.Drawing.Point(288, 31);
+            this.btnGenerer.Name = "btnGenerer";
+            this.btnGenerer.Size = new System.Drawing.Size(168, 49);
+            this.btnGenerer.TabIndex = 0;
+            this.btnGenerer.Text = "Générer";
+            this.btnGenerer.UseVisualStyleBackColor = true;
+            this.btnGenerer.Click += new System.EventHandler(this.btnGenerer_Click);
+            // 
+            // rtbScenario
+            // 
+            this.rtbScenario.Location = new System.Drawing.Point(149, 125);
+            this.rtbScenario.Name = "rtbScenario";
+            this.rtbScenario.Size = new System.Drawing.Size(440, 186);
+            this.rtbScenario.TabIndex = 1;
+            this.rtbScenario.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbScenario);
+            this.Controls.Add(this.btnGenerer);
+            this.Name = "Form1";
+            this.Text = "Générateur de scénario";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerer;
+        private System.Windows.Forms.RichTextBox rtbScenario;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateurDeScenario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerer_Click(object sender, EventArgs e)
+        {
+            // Listes d’éléments pour la génération
+            string[] personnages = { "Un détective alcoolique", "Une archéologue rebelle", "Un alien végétarien", "Un hacker de 12 ans", "Un dragon introverti" };
+            string[] objectifs = { "doit sauver la ville", "cherche sa véritable identité", "tente de gagner un concours de danse", "prévoit de voler la lune", "doit éviter une guerre mondiale" };
+            string[] lieux = { "dans un monde post-apocalyptique", "dans une école pour monstres", "dans un cybercafé abandonné", "dans un royaume sous-marin", "sur une île volante" };
+            string[] antagonistes = { "face à un robot psychopathe", "traqué par ses propres souvenirs", "contre une secte millénaire", "opposé à un bébé démoniaque", "poursuivi par un double maléfique" };
+
+            // Génération aléatoire
+            Random rnd = new Random();
+            string p = personnages[rnd.Next(personnages.Length)];
+            string o = objectifs[rnd.Next(objectifs.Length)];
+            string l = lieux[rnd.Next(lieux.Length)];
+            string a = antagonistes[rnd.Next(antagonistes.Length)];
+
+            // Construction du scénario
+            string scenario = $"{p} {o} {l}, {a}.";
+
+            // Affichage
+            rtbScenario.Text = scenario;
+        
+    }
+    }
+}
